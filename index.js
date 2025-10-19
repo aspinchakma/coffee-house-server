ace1f8916077c05f2c3dec16c3b0cd1f742ddd7f
Aspin Chakma <aspinchakma2019@gmail.com>
2025-10-19 13:56:29 +0600

    fixing error

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ require("dotenv").config();
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
-// const cors = require("cors");
-app.use(cors({ origin: "*" }));
-
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Connecting to the database
